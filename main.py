cdeafc94686815d9e54c663ee94dc586e0c590a1
anna <annabsnss@gmail.com>
2024-06-17 14:49:10 -0300

    atualizações no main

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from pygame.locals import *
 from player import Player
+from button import Button
 
 pygame.init()
 
@@ -15,9 +16,22 @@ pygame.display.set_caption("decoding your fears: Clara's path")
 
 # game variables
 tile_size = 40
+main_menu = True
 
 # load images
-bg_img = pygame.image.load('assets/background.jpg')
+bg_img = pygame.image.load('assets/background_start.png')
+startbtn_img = pygame.image.load('assets/start.png')
+exitbtn_img = pygame.image.load('assets/exit.png')
+music_on_img = pygame.image.load('assets/music_on.png')
+music_off_img = pygame.image.load('assets/music_off.png')
+
+# load sounds
+pygame.mixer.music.set_volume(0.5)
+bg_sound = pygame.mixer.music.load('assets/desafio.mp3')
+pygame.mixer.music.play(-1)
+
+button_sound = pygame.mixer.Sound('assets/botao.wav')
+button_sound.set_volume(0.2)
 
 class World():
 	def __init__(self, data):
@@ -76,13 +90,49 @@ world_data = [
 player = Player(100, screen_height - 130)
 world = World(world_data)
 
+#create buttons
+start_button = Button(screen_width // 2 - 90, screen_height // 2 , startbtn_img)
+exit_button = Button(screen_width // 2 - 90, screen_height // 2 + 60, exitbtn_img)
+music_on_button = Button(screen_width // 2 - 40, screen_height // 2 + 120, music_on_img)
+music_off_button = Button(screen_width // 2 + 15, screen_height // 2 + 120, music_off_img)
+
+
 running = True
 while running:
 	clock.tick(fps)
 	screen.blit(bg_img, (0, -25))
-	world.draw()
+	
+	if main_menu == True:
+		if exit_button.drawbutton():
+			button_sound.play()
+			running = False
+		if start_button.drawbutton():
+			button_sound.play()
+			pygame.mixer.music.fadeout(1)
+			pygame.mixer.music.stop()
+			pygame.mixer.music.set_volume(0.1)
+			pygame.mixer.music.load('assets/level1.mp3')  # Carregar a nova música
+			pygame.mixer.music.play(-1)
+			main_menu = False
+			bg_img = pygame.image.load('assets/background.jpg')
+		if music_on_button.drawbutton():
+			button_sound.play()
+			pygame.mixer.music.play(-1)
+		if music_off_button.drawbutton():
+			button_sound.play()
+			pygame.mixer.music.stop()
 
-	player.update(screen, screen_height, world)  # Passa 'world' como argumento para o método update()
+	else:
+		world.draw()
+	
+		music_on_button = Button(screen_width // 2 + 350, screen_height // 2 + 270, music_on_img)
+		music_off_button = Button(screen_width // 2 + 350, screen_height // 2 + 320, music_off_img)
+		if music_on_button.drawbutton():
+			pygame.mixer.music.play(-1)
+		if music_off_button.drawbutton():
+			pygame.mixer.music.stop()
+	
+		player.update(screen, screen_height, world)  # Passa 'world' como argumento para o método update()
 
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
