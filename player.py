b7f33533c78f153bd2450306d9e3d50546be4cf8
Ana Christine Ferreira Costa <92758784+AnaChris154@users.noreply.github.com>
2024-06-18 17:47:22 -0300

    Ajustando bug de pulo

--- player.py
+++ player.py
@@ -1,96 +1,99 @@
 import pygame
 
-
 class Player():
-	def __init__(self, x, y):
-		self.images_right = []
-		self.images_left = []
-		self.index = 0
-		self.counter = 0
-		for num in range(1, 4):
-			img_right = pygame.image.load(f'assets/girl{num}.png')
-			img_right = pygame.transform.scale(img_right, (40, 80))
-			img_left = pygame.transform.flip(img_right, True, False)
-			self.images_right.append(img_right)
-			self.images_left.append(img_left)
+    def __init__(self, x, y):
+        self.images_right = []
+        self.images_left = []
+        self.index = 0
+        self.counter = 0
+        for num in range(1, 4):
+            img_right = pygame.image.load(f'assets/girl{num}.png')
+            img_right = pygame.transform.scale(img_right, (40, 80))
+            img_left = pygame.transform.flip(img_right, True, False)
+            self.images_right.append(img_right)
+            self.images_left.append(img_left)
 
-		self.image = self.images_right[self.index]     
-		self.rect = self.image.get_rect()
-		self.rect.x = x
-		self.rect.y = y
-		self.width = self.image.get_width()
-		self.height = self.image.get_height()
-		self.vel_y = 0 
-		self.jumped = False
-		self.direction = 0
-	
-	def update(self, screen, screen_height, world):
-		dx = 0
-		dy = 0
-		walk_cooldown = 5
+        self.image = self.images_right[self.index]
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.width = self.image.get_width()
+        self.height = self.image.get_height()
+        self.vel_y = 0
+        self.jumped = False
+        self.jump_count = 0
+        self.direction = 0
 
-		#pega o comando das teclas
-		key = pygame.key.get_pressed()
-		if key[pygame.K_SPACE] and self.jumped==False:
-			self.vel_y = -12
-			self.jumped = True
-		if key[pygame.K_SPACE]==False:
-			self.jumped = False
-		if key[pygame.K_LEFT]:
-			dx -= 5
-			self.counter += 1
-			self.direction = -1
-		if key[pygame.K_RIGHT]:
-			dx += 5
-			self.counter += 1
-			self.direction = 1
-		if key[pygame.K_LEFT] == False and key[pygame.K_RIGHT] ==False:
-			self.counter = 0
-			self.index = 0
-			if self.direction == 1:
-				self.image = self.images_right[self.index]
-			if self.direction == -1:
-				self.image = self.images_left[self.index]
+    def update(self, screen, screen_height, world):
+        dx = 0
+        dy = 0
+        walk_cooldown = 5
 
-		# adiciona gravidade
-		self.vel_y += 1
-		if self.vel_y > 10:
-			self.vel_y = 10
-		dy += self.vel_y
+        # Pega os comandos das teclas
+        key = pygame.key.get_pressed()
+        if key[pygame.K_SPACE]:
+            if not self.jumped and self.jump_count < 2:
+                self.vel_y = -12
+                self.jumped = True
+                self.jump_count += 1
+        else:
+            self.jumped = False
 
-		# manipular a animação
-		if self.counter > walk_cooldown:
-			self.counter = 0
-			self.index += 1
-			if self.index >= len(self.images_right):
-					self.index = 0
-			if self.direction == 1:
-				self.image = self.images_right[self.index]
-			if self.direction == -1:
-				self.image = self.images_left[self.index]
+        if key[pygame.K_LEFT]:
+            dx -= 5
+            self.counter += 1
+            self.direction = -1
+        if key[pygame.K_RIGHT]:
+            dx += 5
+            self.counter += 1
+            self.direction = 1
+        if not key[pygame.K_LEFT] and not key[pygame.K_RIGHT]:
+            self.counter = 0
+            self.index = 0
+            if self.direction == 1:
+                self.image = self.images_right[self.index]
+            if self.direction == -1:
+                self.image = self.images_left[self.index]
 
-		# checando a colisão
-		for tile in world.tile_list:
-			#checando a colisão na direção x
-			if tile[1].colliderect(self.rect.x + dx, self.rect.y, self.width, self.height):
-				dx = 0
-			#checanodo a colisão na direção y
-			if tile[1].colliderect(self.rect.x, self.rect.y + dy, self.width, self.height):
-				if self.vel_y < 0:
-					dy = tile[1].bottom - self.rect.top
+        # Adiciona gravidade
+        self.vel_y += 1
+        if self.vel_y > 10:
+            self.vel_y = 10
+        dy += self.vel_y
 
-			if tile[1].colliderect(self.rect.x, self.rect.y + dy, self.width, self.height):
-				if self.vel_y >= 0:
-					dy = tile[1].top - self.rect.bottom
+        # Manipular a animação
+        if self.counter > walk_cooldown:
+            self.counter = 0
+            self.index += 1
+            if self.index >= len(self.images_right):
+                self.index = 0
+            if self.direction == 1:
+                self.image = self.images_right[self.index]
+            if self.direction == -1:
+                self.image = self.images_left[self.index]
 
-		# atualiza as coordenadas do player
-		self.rect.x += dx
-		self.rect.y += dy
+        # Checando a colisão
+        for tile in world.tile_list:
+            # Checando a colisão na direção x
+            if tile[1].colliderect(self.rect.x + dx, self.rect.y, self.width, self.height):
+                dx = 0
+            # Checando a colisão na direção y
+            if tile[1].colliderect(self.rect.x, self.rect.y + dy, self.width, self.height):
+                if self.vel_y < 0:
+                    dy = tile[1].bottom - self.rect.top
+                elif self.vel_y >= 0:
+                    dy = tile[1].top - self.rect.bottom
+                    self.vel_y = 0
+                    self.jump_count = 0  # Reseta o contador de pulos ao tocar o chão
 
-		if self.rect.bottom > screen_height:
-			self.rect.bottom = screen_height
-			self.vel_y = 0
+        # Atualiza as coordenadas do player
+        self.rect.x += dx
+        self.rect.y += dy
 
-		# coloca o player na tela
-		screen.blit(self.image, self.rect)
-		
+        if self.rect.bottom > screen_height:
+            self.rect.bottom = screen_height
+            self.vel_y = 0
+            self.jump_count = 0  # Reseta o contador de pulos ao tocar o fundo da tela
+
+        # Coloca o player na tela
+        screen.blit(self.image, self.rect)
