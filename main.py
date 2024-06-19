ba8e0dd12049177895502689dc32331ee686e8c9
anna <annabsnss@gmail.com>
2024-06-18 21:19:48 -0300

    teste_anna -> main

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from world import World
 from button import Button
 
 
-
 pygame.init()
 
 clock = pygame.time.Clock()
@@ -17,6 +16,15 @@ screen_height = 800
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("decoding your fears: Clara's path")
 
+# uteis para entrada de texto
+base_font = pygame.font.Font(None, 30)
+user_text = ''
+input_rect = pygame.Rect(screen_width // 2 - 90, screen_height // 2 - 2, 175, 45)
+input_rect2 = pygame.Rect(screen_width // 2 - 92, screen_height // 2 - 5, 180, 50)
+rect_color = (80, 80, 80)
+
+active = False
+nickname_entered = False
 main_menu = True
 game_over=0
 
@@ -24,8 +32,10 @@ game_over=0
 bg_img = pygame.image.load('assets/background_start.png')
 startbtn_img = pygame.image.load('assets/start.png')
 exitbtn_img = pygame.image.load('assets/exit.png')
+submitbtn_img = pygame.image.load('assets/submit.png')
 music_on_img = pygame.image.load('assets/music_on.png')
 music_off_img = pygame.image.load('assets/music_off.png')
+restartbtn_img = pygame.image.load('assets/restart.png')
 
 # load sounds
 pygame.mixer.music.set_volume(0.5)
@@ -34,7 +44,6 @@ pygame.mixer.music.play(-1)
 
 button_sound = pygame.mixer.Sound('assets/botao.wav')
 button_sound.set_volume(0.2)
-
 
 world_data = [
 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
@@ -62,12 +71,13 @@ world_data = [
 player = Player(100, screen_height - 130)
 world = World(world_data)
 
-
 #create buttons
-start_button = Button(screen_width // 2 - 90, screen_height // 2 , startbtn_img)
-exit_button = Button(screen_width // 2 - 90, screen_height // 2 + 60, exitbtn_img)
-music_on_button = Button(screen_width // 2 - 40, screen_height // 2 + 120, music_on_img)
-music_off_button = Button(screen_width // 2 + 15, screen_height // 2 + 120, music_off_img)
+start_button = Button(screen_width // 2 - 98, screen_height // 2 - 5, startbtn_img)
+exit_button = Button(screen_width // 2 - 98, screen_height // 2 + 55, exitbtn_img)
+submit_button = Button(screen_width // 2 - 20, screen_height // 2 + 50, submitbtn_img)
+music_on_button = Button(screen_width // 2 + 350, screen_height // 2 + 270, music_on_img)
+music_off_button = Button(screen_width // 2 + 350, screen_height // 2 + 320, music_off_img)
+restart_button = Button(screen_width // 2 - 98, screen_height // 2, restartbtn_img)
 
 running = True
 while running:
@@ -75,46 +85,75 @@ while running:
 	screen.blit(bg_img, (0, -25))
 
 	if main_menu == True:
-		if exit_button.drawbutton():
-			button_sound.play()
-			running = False
-		if start_button.drawbutton():
-			button_sound.play()
-			pygame.mixer.music.fadeout(1)
-			pygame.mixer.music.stop()
-			pygame.mixer.music.set_volume(0.1)
-			pygame.mixer.music.load('assets/level1.mp3')  # Carregar a nova música
-			pygame.mixer.music.play(-1)
-			main_menu = False
-			bg_img = pygame.image.load('assets/background.jpg')
-		if music_on_button.drawbutton():
-			button_sound.play()
-			pygame.mixer.music.play(-1)
-		if music_off_button.drawbutton():
-			button_sound.play()
-			pygame.mixer.music.stop()
+		if not nickname_entered:
+			if start_button.drawbutton():
+				button_sound.play()
+				nickname_entered = True
+			if exit_button.drawbutton():
+				button_sound.play()
+				running = False
+			
+		else:
+			pygame.draw.rect(screen, (200, 200, 200), input_rect, 0, 90)
+			pygame.draw.rect(screen, rect_color, input_rect2, 5, 90)
+			text_surface = base_font.render(user_text, True, (0, 0, 0))
+			screen.blit(text_surface, (input_rect.x + 15, input_rect.y + 12))
+
+			if submit_button.drawbutton():
+				button_sound.play()
+				main_menu = False
+				# Carregar a nova música
+				pygame.mixer.music.stop()
+				pygame.mixer.music.set_volume(0.1)
+				pygame.mixer.music.load('assets/level1.mp3')
+				pygame.mixer.music.play(-1)
+				bg_img = pygame.image.load('assets/background.jpg')
 
 	else: 
-		
 		world.draw()
 		world.draw_enemies()
-		music_on_button = Button(screen_width // 2 + 350, screen_height // 2 + 270, music_on_img)
-		music_off_button = Button(screen_width // 2 + 350, screen_height // 2 + 320, music_off_img)
-		if music_on_button.drawbutton():
-			pygame.mixer.music.play(-1)
-		if music_off_button.drawbutton():
-			pygame.mixer.music.stop()
+
 		if game_over==0:
 			world.update_enemies()
 
-		game_over= player.update(screen, screen_height, world, game_over)  # Passa 'world' como argumento para o método update()
-	
-	
+		game_over = player.update(screen, screen_height, world, game_over)  # Passa 'world' como argumento para o método update()
 
+		if game_over == -1:
+			pygame.mixer.music.stop()
+			if restart_button.drawbutton():
+				button_sound.play()
+				player = Player(100, screen_height - 130)  # Reseta o jogador
+				world = World(world_data)  # Reseta o mundo
+				game_over = 0
+				pygame.mixer.music.play(-1)
+
+	if music_on_button.drawbutton():
+		button_sound.play()
+		pygame.mixer.music.play(-1)
+	if music_off_button.drawbutton():
+		button_sound.play()
+		pygame.mixer.music.stop()	
 
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			running = False
+
+		if event.type == pygame.MOUSEBUTTONDOWN:
+			if input_rect.collidepoint(event.pos):
+				active = True
+				rect_color = (56, 56, 56)
+			else:
+				active = False
+				rect_color = (70, 110, 119)
+
+		if event.type == pygame.KEYDOWN:
+			if active and nickname_entered:
+				if event.key == pygame.K_BACKSPACE:
+					user_text = user_text[:-1]
+				else:
+					# Limitar o tamanho do texto ao tamanho do retângulo
+					if base_font.size(user_text + event.unicode)[0] < input_rect.width - 20:
+						user_text += event.unicode
 
 	pygame.display.update()
 
